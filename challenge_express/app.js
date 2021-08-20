d8b71bd9545c957fa8a064955c3081c31a7cf076
Isabela Medeiros <isabela.medeiros@ibm.com>
2021-08-20 00:56:12 -0300

    Adicionando o challenge_express

--- challenge_express/app.js
+++ challenge_express/app.js
@@ -1,34 +1,15 @@
+const path = require('path')
 
-const http = require('http');
 const express = require('express')
+
+const mainRoutes = require('./routes/index')
 
 const app = express();
 
-const server = http.createServer(app);
+app.use(express.static(path.join(__dirname, 'public')))
 
-server.listen(3000);
+app.use(mainRoutes)
 
-console.log('===> Port 3000');
-//
-// const express = require('express');
+app.listen(3000);
 
-// const app = express();
-
-// app.use('/', req, res, next){
-
-// console.log("Middleware 01");
-
-// res.send('Resposta do Middleware 01');
-
-// }
-
-// app.use('/users', req, res, next){
-
-// console.log("Middleware 02");
-
-// res.send('Resposta do Middleware 02');
-
-// }
-
-// app.listen(3000);
-
+console.log('==> Port 3000');
